ed70a69c6735e9116ed5dd0245d278bf2fc3f368
aslisabanci <aslisabanci@gmail.com>
2020-08-19 18:22:35 -0700

    Reverted to print as logging.info messages are not shown on action logs

--- action_src/algorithmia_utils.py
+++ action_src/algorithmia_utils.py
@@ -1,6 +1,5 @@
 import Algorithmia
-import logging
 
 
 def upload_model(local_path, remote_path):
-    logging.info("will upload model from {} to {}".format(local_path, remote_path))
+    print("will upload model from {} to {}".format(local_path, remote_path))
--- entrypoint.py
+++ entrypoint.py
@@ -2,7 +2,6 @@
 
 import os, json
 from action_src import algorithmia_utils, notebook_utils
-import logging
 
 if __name__ == "__main__":
     repo_name = os.getenv("INPUT_CURRENT_REPO")
@@ -13,7 +12,7 @@ if __name__ == "__main__":
 
     algorithmia_api_key = os.getenv("INPUT_ALGORITHMIA_API_KEY")
     notebook_path = os.getenv("INPUT_NOTEBOOK_PATH")
-    logging.info("notebook path:", notebook_path)
+    print("notebook path:", notebook_path)
     remote_upload_path = os.getenv("INPUT_REMOTE_UPLOAD_PATH")
     model_file_path = os.getenv("INPUT_MODEL_FILE_PATH")
 
@@ -23,7 +22,7 @@ if __name__ == "__main__":
 
     if os.path.exists(repo_path):
         workspace_notebook_path = "{}/{}".format(repo_path, notebook_path)
-        logging.info("workspace notebook path:", workspace_notebook_path)
+        print("workspace notebook path:", workspace_notebook_path)
         notebook_utils.run_notebook(
             notebook_path=workspace_notebook_path, execution_path=repo_path
         )
@@ -31,7 +30,7 @@ if __name__ == "__main__":
         notebook_output_file = "{}/test_out.txt".format(repo_path)
         with open(notebook_output_file) as f:
             test_file_contents = f.read()
-            logging.info(test_file_contents)
+            print(test_file_contents)
     else:
         raise Exception(
             "actions/checkout on the local repo must be run before this action can be completed"
