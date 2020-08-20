050d1ea4297d056bcc5512f9fef7d0957a25b310
aslisabanci <aslisabanci@gmail.com>
2020-08-19 18:31:41 -0700

    Removed kernel_name param

--- action_src/notebook_utils.py
+++ action_src/notebook_utils.py
@@ -6,7 +6,7 @@ def run_notebook(notebook_path, execution_path):
     print("notebook and exec paths:", notebook_path, execution_path)
     with open(notebook_path) as f:
         nb = nbformat.read(f, as_version=4)
-        ep = ExecutePreprocessor(timeout=600, kernel_name="python3")
+        ep = ExecutePreprocessor(timeout=600)
         ep.preprocess(
             nb, {"metadata": {"path": execution_path}},
         )
