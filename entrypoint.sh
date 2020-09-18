c12559f1aba9a4b46a4caa42c6a2ada8d697a3e3
aslisabanci <aslisabanci@gmail.com>
2020-09-17 23:34:50 -0700

    Testing entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,5 @@
 # or splits `args` in a string separated by whitespace.
 pwd
 ls
+git clone https://"$ALGORITHMIA_USERNAME":"$ALGORITHMIA_PASSWORD"@git.algorithmia.com/git/"$ALGORITHMIA_USERNAME"/"$ALGORITHMIA_ALGONAME".git
 python3 /entrypoint.py
