78c1e586235351a3bba2ad7a3f41ad5e2a8a949c
aslisabanci <aslisabanci@gmail.com>
2020-09-17 23:16:42 -0700

    Testing entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 # `$*` expands the `args` supplied in an `array` individually 
 # or splits `args` in a string separated by whitespace.
-sh -c "echo $*"
 pwd
 ls
-python3 ./entrypoint.py
+python3 /entrypoint.py
