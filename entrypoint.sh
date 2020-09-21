546a377c5e44344eb7b45d0963d1acbfb2153b27
aslisabanci <aslisabanci@gmail.com>
2020-09-21 13:27:44 -0700

    Assume algodir=algoname

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,8 @@ if [ $? -eq 0 ]
 then
     echo "Successfully executed action script, for optional notebook execution and model file upload."
 
-    cp -R "$INPUT_ALGORITHMIA_ALGO_DIR"/. "$INPUT_ALGORITHMIA_ALGONAME"/
+    #cp -R "$INPUT_ALGORITHMIA_ALGO_DIR"/. "$INPUT_ALGORITHMIA_ALGONAME"/
+    cp -R "$INPUT_ALGORITHMIA_ALGONAME"/. "$INPUT_ALGORITHMIA_ALGONAME"/
     cd "$INPUT_ALGORITHMIA_ALGONAME"
     git config --global user.name "$INPUT_ALGORITHMIA_USERNAME"
     git config --global user.email "$INPUT_ALGORITHMIA_EMAIL"
