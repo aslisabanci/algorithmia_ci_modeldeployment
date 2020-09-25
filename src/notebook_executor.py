016fc1d4839ea3b88d0bf7fee02d37d1bcf1b0bf
aslisabanci <aslisabanci@gmail.com>
2020-09-24 22:02:26 -0700

    Renamed default notebook name

--- src/notebook_executor.py
+++ src/notebook_executor.py
@@ -3,17 +3,6 @@ import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
 from nbconvert.preprocessors.execute import CellExecutionError
 
-# def run_notebook(notebook_path, execution_path):
-#     print("notebook and exec paths:", notebook_path, execution_path)
-#     with open(notebook_path) as f:
-#         nb = nbformat.read(f, as_version=4)
-#         ep = ExecutePreprocessor(timeout=600)
-#         ep.preprocess(
-#             nb,
-#             {"metadata": {"path": execution_path}},
-#         )
-#     print("done with notebook execution")
-
 
 class NotebookExecutor:
     def __init__(self, workspace_path, notebook_path, timeout=600):
@@ -37,3 +26,24 @@ class NotebookExecutor:
             print(
                 f"Notebook file not found at path: {self.notebook_full_path}. Omitting notebook execution step."
             )
+
+
+import nbformat
+from nbconvert import PythonExporter
+
+
+def convertNotebook(notebookPath, modulePath):
+    with open(notebookPath) as nbfile:
+        nb = nbformat.reads(nbfile.read(), nbformat.NO_CONVERT)
+
+    exporter = PythonExporter()
+    source, _ = exporter.from_notebook_node(nb)
+
+    with open(modulePath, "w+") as nbfile:
+        nbfile.writelines(source)
+
+
+convertNotebook(
+    "/Users/aslisabanci/repos/algorithmia_ci/demo_autodeploy_algo_on_algorithmia/demo_algo.ipynb",
+    "/Users/aslisabanci/Desktop/stripped_notebook_algo.py",
+)
