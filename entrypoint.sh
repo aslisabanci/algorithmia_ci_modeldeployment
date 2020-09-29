12b3477e4989cdd9c508d791fe8dafdab36c7616
aslisabanci <aslisabanci@gmail.com>
2020-09-28 22:55:00 -0700

    Appended username to github pat for algo cloning

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,10 @@ pip3 install -r requirements.txt
 # Clone the Algorithmia algorithm repository
 CI_ALGO_DIR=$INPUT_ALGORITHMIA_ALGONAME"_CI"
 
-if [ -z "$ALGORITHMIA_PASSWORD" ]
+if [ -z "$INPUT_ALGORITHMIA_PASSWORD" ]
 then
     echo "Will clone algorithm repository hosted on Github"
-    git clone https://"$GITHUB_PAT"@github.com/aslisabanci/xgboost_automated_github.git $CI_ALGO_DIR
+    git clone https://"$INPUT_GITHUB_PAT"@github.com/"$INPUT_GITHUB_USERNAME"/"$INPUT_ALGORITHMIA_ALGONAME".git $CI_ALGO_DIR
 else
     echo "Will clone algorithm repository hosted on Algorithmia"
     git clone https://"$INPUT_ALGORITHMIA_USERNAME":"$INPUT_ALGORITHMIA_PASSWORD"@git.algorithmia.com/git/"$INPUT_ALGORITHMIA_USERNAME"/"$INPUT_ALGORITHMIA_ALGONAME".git $CI_ALGO_DIR
